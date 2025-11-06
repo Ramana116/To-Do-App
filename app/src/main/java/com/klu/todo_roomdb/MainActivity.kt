f59af3c9129782b5811e2ed43d4b21b49cd36f5c
Ramana116 <ramanagajulavarthi116@gmail.com>
2025-11-06 21:46:00 +0530

    Initial commit - added project files

---
+++ app/src/main/java/com/klu/todo_roomdb/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.klu.todo_roomdb
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.klu.todo_roomdb.ui.theme.Todo_roomdbTheme
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+        setContent {
+            Todo_roomdbTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TodoListPage(todoViewModel)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/klu/todo_roomdb/db/TodoDao.kt
@@ -0,0 +1,23 @@
+package com.klu.todo_roomdb.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.klu.todo_roomdb.Todo
+
+
+
+@Dao
+interface TodoDao {
+
+    @Query("SELECT * FROM Todo ORDER BY createdAt DESC")
+    fun getAllTodo() : LiveData<List<Todo>>
+
+    @Insert
+    fun addTodo(todo : Todo)
+
+    @Query("Delete FROM Todo where id = :id")
+    fun deleteTodo(id : Int)
+
+}
